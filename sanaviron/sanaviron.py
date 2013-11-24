6f3c9fc131cfbd65fe957e67f61d97d8eb0dd9a5
Ivlev Denis <ivlevdenis@sanaviron.org>
2013-11-24 17:14:08 +0400

    Some changes

--- sanaviron/sanaviron.py
+++ sanaviron/sanaviron.py
@@ -7,19 +7,10 @@ Main Sanaviron
 
 import platform
 import sys
-import gtk # because Linux run threads_init
+import gtk  # because Linux run threads_init
 import os
 
 from __init__ import *
-#sys.path.append('..')
-#from sanaviron import set_locale
-
-#if platform.system() == 'Windows':
-#    set_locale()
-#else:
-#    gtk.threads_init()
-#
-#install_gettext("sanaviron")
 
 def startapp():
     if platform.system() == 'Windows':
@@ -41,7 +32,8 @@ def startapp():
     application = Application()
 
     if '--sample' in sys.argv:
-        application.editor.canvas.load_from_xml(os.path.join("..", "examples", "invoice.xml"))
+        application.editor.canvas.load_from_xml(
+            os.path.join("..", "examples", "invoice.xml"))
 
     application.run()
 
